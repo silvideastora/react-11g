e93df57fef977ef68713c53af97027f01cb0b18e
David Moranchel <david.arcshtag@gmail.com>
2021-06-03 20:58:10 -0500

    refact: Refactor AppThemeToggle componente class to function

--- src/components/AppThemeToggle.jsx
+++ src/components/AppThemeToggle.jsx
@@ -1,49 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import './appThemeToggle.css';
+import "./appThemeToggle.css";
 // import styles from './appThemeToggle.css';
 
-class AppThemeToggle extends React.Component {
-    constructor(props) {
-        super(props)
+// class AppThemeToggle extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-        this.state = { theme: 'light' };
+//     this.state = { theme: "light" };
 
-        this.toggle = this.toggle.bind(this)
-        
-        console.log('CONSTRUCTOR')
-    }
+//     this.toggle = this.toggle.bind(this);
 
-    toggle() {
-        const theme = this.state.theme === 'light'
-            ? 'dark'
-            : 'light';
+//     console.log("CONSTRUCTOR");
+//   }
 
-        this.setState({ theme });
-    }
+//   toggle() {
+//     const theme = this.state.theme === "light" ? "dark" : "light";
 
-    render() {
-        console.log('RENDER')
-        return (
-            <div
-                className={`appThemeToggle d-flex align-items-center justify-content-center ${this.state.theme}`}
-                // className={`${styles.appThemeToggle} d-flex align-items-center justify-content-center ${this.state.theme}`}
-                onClick={this.toggle}
-                
-            />
-        )
-    }
+//     this.setState({ theme });
+//   }
 
-    componentDidMount() {
-        console.log('DID MOUNT')
-        document.body.setAttribute('theme', this.state.theme);
-    }
+//   render() {
+//     console.log("RENDER");
+//     return (
+//       <div
+//         className={`appThemeToggle d-flex align-items-center justify-content-center ${this.state.theme}`}
+//         // className={`${styles.appThemeToggle} d-flex align-items-center justify-content-center ${this.state.theme}`}
+//         onClick={this.toggle}
+//       />
+//     );
+//   }
 
+//   componentDidMount() {
+//     console.log("DID MOUNT");
+//     document.body.setAttribute("theme", this.state.theme);
+//   }
 
-    componentDidUpdate() {
-        console.log('DID UPDATE')
-        document.body.setAttribute('theme', this.state.theme);
-    }
+//   componentDidUpdate() {
+//     console.log("DID UPDATE");
+//     document.body.setAttribute("theme", this.state.theme);
+//   }
+// }
+
+function AppThemeToggle() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.setAttribute("theme", theme);
+  }, []);
+
+  useEffect(() => {
+    document.body.setAttribute("theme", theme);
+  }, [theme]);
+
+  const toggle = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <div
+      className={`appThemeToggle d-flex align-items-center justify-content-center ${theme}`}
+      onClick={toggle}
+    />
+  );
 }
 
 export default AppThemeToggle;
