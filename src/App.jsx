30e33f0c12e61ae8469c64adfb41d97b29b1f93b
David Moranchel <david.arcshtag@gmail.com>
2021-06-03 20:51:28 -0500

    refact: Refactor AppLoading componente class to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import AppLoading from './components/AppLoading';
-import Header from './components/Header';
-import SideNavLeft from './components/SideNavLeft';
-import SideNavRight from './components/SideNavRight';
-import Posts from './components/Posts';
-import Counter from './components/Counter';
+import AppLoading from "./components/AppLoading";
+import Header from "./components/Header";
+import SideNavLeft from "./components/SideNavLeft";
+import SideNavRight from "./components/SideNavRight";
+import Posts from "./components/Posts";
+import Counter from "./components/Counter";
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <Header />
-        
         <div className="container">
           <div className="row">
             <SideNavLeft />
@@ -25,10 +24,10 @@ class App extends React.Component {
             <SideNavRight />
           </div>
         </div>
-
         <AppLoading />
+        ∏∏
       </div>
-    )
+    );
   }
 }
 
--- src/components/AppLoading.jsx
+++ src/components/AppLoading.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import './appLoading.css';
+import "./appLoading.css";
 
-class AppLoading extends React.Component {
-    constructor(props) {
-        super(props)
+// class AppLoading extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-        this.state = { active: true };
-    }
+//     this.state = { active: true };
+//   }
 
-    render() {
-        const active = this.state.active ? 'active' : '';
+//   render() {
+//     const active = this.state.active ? "active" : "";
 
-        return (
-            <div className={`loading ${active} align-items-center justify-content-center`}>
-                <div className="spinner-border" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-            </div>
-        )
-    }
+//     return (
+//       <div
+//         className={`loading ${active} align-items-center justify-content-center`}
+//       >
+//         <div className="spinner-border" role="status">
+//           <span className="visually-hidden">Loading...</span>
+//         </div>
+//       </div>
+//     );
+//   }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({ active: false });
-        }, 400);
-    }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ active: false });
+//     }, 400);
+//   }
+// }
+
+function AppLoading() {
+  const [isActive, setIsActive] = useState(true);
+  const active = isActive ? "active" : "";
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsActive(false);
+    }, 400);
+  }, []);
+
+  return (
+    <div
+      className={`loading ${active} align-items-center justify-content-center`}
+    >
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
 }
 
-export default AppLoading
+export default AppLoading;
