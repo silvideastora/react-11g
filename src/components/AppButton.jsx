ab9dede8c3e18012ccdc3656813d2fc3eabd6852
David Moranchel <david.arcshtag@gmail.com>
2021-06-03 19:38:26 -0500

    refact: Refactor AppButton componente class to function

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React from "react";
 
 // import Styles from './AppButon.module.css'
 
-class AppButton extends React.Component {
-    constructor(props) {
-        super(props);
+// class AppButton extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-        this.props = props;
-    }
+//     this.props = props;
+//   }
 
-    render() {
-        const className = this.props.className ? this.props.className : '';
+//   render() {
+//     const className = this.props.className ? this.props.className : "";
 
-        if (this.props.type === 'anchor') {
-            return (
-                <a className={`btn btn-link ${className}`}>
-                {/* // <a className={`btn btn-link ${className.anchor}`}> */}
-                    {this.props.text}
-                </a>
-            )
-        }
+//     if (this.props.type === "anchor") {
+//       return (
+//         <a className={`btn btn-link ${className}`}>
+//           {/* // <a className={`btn btn-link ${className.anchor}`}> */}
+//           {this.props.text}
+//         </a>
+//       );
+//     }
 
-        return (
-            <button
-                className={`btn ${className}`}
-                type="button"
-            >
-                {this.props.text}
-            </button>
-        )
-    }
+//     return (
+//       <button className={`btn ${className}`} type="button">
+//         {this.props.text}
+//       </button>
+//     );
+//   }
+// }
+
+function AppButton({ className: classNameProp, type, text }) {
+  const className = classNameProp ? classNameProp : "";
+
+  if (type === "anchor") {
+    return <a className={`btn btn-link ${className}`}>{text}</a>;
+  }
+
+  return (
+    <button className={`btn ${className}`} type="button">
+      {text}
+    </button>
+  );
 }
 
-export default AppButton
+export default AppButton;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,46 +2,44 @@ import React, { useState, useEffect } from "react";
 
 function Counter() {
   const [counter, setCounter] = useState(0);
-  const [counterTwo, setCounterTwo] = useState(0)
-
-
-  useEffect(() => {
-    console.log("me ejecuto cuando se monta el componente")
-  }, [])
+  const [counterTwo, setCounterTwo] = useState(0);
 
   useEffect(() => {
-    console.log("me ejecuto cuando cambia counter")
-  }, [counter])
+    console.log("me ejecuto cuando se monta el componente");
+  }, []);
 
   useEffect(() => {
-    console.log("me ejecuto cuando cambia counterTwo")
-  }, [counterTwo])
+    console.log("me ejecuto cuando cambia counter");
+  }, [counter]);
 
   useEffect(() => {
-    console.log("me ejecuto siempre")
-  })
+    console.log("me ejecuto cuando cambia counterTwo");
+  }, [counterTwo]);
 
+  useEffect(() => {
+    console.log("me ejecuto siempre");
+  });
 
   const modifyCounter = (action) => {
     if (action === "increment") {
       const newCounter = counter + 1;
       setCounter(newCounter);
-      return
+      return;
     }
     const newCounter = counter - 1;
     setCounter(newCounter);
-    return
+    return;
   };
 
   const modifyCounterTwo = (action) => {
     if (action === "increment") {
       const newCounter = counterTwo + 1;
       setCounterTwo(newCounter);
-      return
+      return;
     }
     const newCounter = counterTwo - 1;
     setCounterTwo(newCounter);
-    return
+    return;
   };
 
   return (
